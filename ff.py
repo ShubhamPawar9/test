934de410347f85e31a32e58588037456a6ccf86b
ShubhamPawar9 <86544252+ShubhamPawar9@users.noreply.github.com>
2021-07-01 22:24:59 +0530

    Update ff.py

--- ff.py
+++ ff.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 sitemap = open(r'C:\Users\Shubham\Documents\sitemap925.txt', 'r')
 
 for io in sitemap.readlines():
-	reqs = requests.get(io)
+	reqs = requests.get((io).replace('\n',''))
+
 	soup = BeautifulSoup(reqs.text, 'html.parser')
-	print(soup)
 
 	for link in soup.find_all('a'):
 		ok = link.get('href')
 		if '9to5mac.com' not in ok:
-			if 'facebook.com' not in ok and 'youtube.com' not in ok and 'twitter.com' not in ok and 'instagram.com' not in ok and 'flipboard.com' not in ok:
+			if 'facebook.com' not in ok and 'youtube.com' not in ok and 'twitter.com' not in ok and 'instagram.com' not in ok and 'flipboard.com' not in ok and '9to5' not in ok:
 				domain = (f'{ok}'.replace('https://','').replace('http://','').replace('https://www.','').replace('http://www.','').replace('www.','').split('/')[0])
 				w = requests.get(f'https://panel.dreamhost.com/marketing/ajax.cgi?cmd=domreg-availability&domain={domain}')
 				y = json.loads(w.text)
